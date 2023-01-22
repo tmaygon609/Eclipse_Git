61f3cfcf9a33b656a5c1884515ad93851385a092
tmaygon609 <tmaygon609@iesalmudeyne.es>
2023-01-22 19:36:40 +0100

    Primer commit en repositorio local

---
+++ src/Prueba/Prueba_Git.java
@@ -0,0 +1,11 @@
+package Prueba;
+
+public class Prueba_Git {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola mundo");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author TibuM
+ *
+ */
+module Eclipse_GitHub {
+}
